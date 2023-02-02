68023e4943ff3e06268532a5ee117fe0835224f6
Enis <enis.mermer.simplon@gmail.com>
2023-02-02 16:46:08 +0100

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log("Wesh Electron, je t'aime trop");
+
+const { app, BrowserWindow } = require('electron');
+
+// inclure le module 'path' de Node.js au début de votre fichier
+const path = require('path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  // la fonction createWindow() appelle une nouvelle fenetre
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  // la fonction app.quit() appelle à fermer une fenetre    
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
