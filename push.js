b04caa0666309b945eba92b72ff42fd2f760eaf6
Enis <enis.mermer.simplon@gmail.com>
2023-02-13 13:45:44 +0100

    push box-sizing

--- push.js
+++ push.js
@@ -4,7 +4,7 @@ push.addEventListener('click', () => {
     Notification.requestPermission().then(perm => {
         if (perm === "granted") {
             const notification = new Notification("OUAISS j'ai réussii", {
-                body: "Achetez n'importe quel amiibo que vous voulez",
+                body: "Achetez n'importe quel amiibo que vous voulez !",
                 data: {hello: "world"},
                 icon: "icon amiibo.png",
                 // tag: "Welcome Message",
