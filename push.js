012de63bde8272b41c17a2849c00cdb17c323686
Enis <enis.mermer.simplon@gmail.com>
2023-02-13 09:31:02 +0100

    update

--- push.js
+++ push.js
@@ -4,7 +4,7 @@ push.addEventListener('click', () => {
     Notification.requestPermission().then(perm => {
         if (perm === "granted") {
             const notification = new Notification("OUAISS j'ai réussii", {
-                body: "amiibo API",
+                body: "Achetez n'importe quel amiibo que vous voulez",
                 data: {hello: "world"},
                 icon: "icon amiibo.png",
                 // tag: "Welcome Message",
